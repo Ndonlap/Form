e8c459e66c8f4017dc1d7af8457f897043db1b62
Ndonlap <162979855+Ndonlap@users.noreply.github.com>
2024-07-08 20:37:53 -0400

    form1

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Display sizes</title>
+</head>
+<body>
+    <h1>welcome to our site</h1>
+<div class="container">
+    <div class="box1">          
+        <div class = "item1">
+            <img src="images/157.jpg" width="100%">
+        </div>
+        <div class = "item2">
+            <img src="images/index.jpeg">
+        </div>
+        <div class = "item3">
+            <img src="images/mich.jpg">
+        </div>
+    </div>
+    <div class="box2">
+        <div class = "item4">
+            <img src="images/i23.jpg">
+        </div>
+        <div class = "item5">
+            <img src="images/qui.jpg">
+        </div>
+        <div class = "item6">
+            <img src="images/mic.jpg">
+        </div>
+    </div>
+</div>
+   
+</body>
+</html>
